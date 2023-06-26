6e988381352850a56c13d3e9cea444d66c6d81ff
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2023-06-27 00:41:23 +1000

    Add links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Head from "next/head";
 import {AiFillLinkedin, AiFillYoutube}  from "react-icons/ai";
+import { FaMedium } from "react-icons/fa";
 import eliza from "./eliza.jpeg"
 
 export default function Home() {
@@ -10,24 +11,31 @@ export default function Home() {
           <h1 className="text-xl">Eliza Fury</h1>
           <ul className="flex items-center">
             <li></li>
-            <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 rounded-md ml-8" href="#">Resume</a></li>
+            <li><a className="bg-gradient-to-r from-blue-500 to-purple-500 text-white px-4 rounded-md ml-8" href="https://www.linkedin.com/in/eliza-fury-3004b3110/">Resume</a></li>
           </ul>
         </nav>
+        <section>
         <div className="text-center p-10">
-          <h1 className="text-5xl py-2 text-teal-600 font-medium">Eliza Fury</h1>
+          <h1 className="text-5xl py-2 text-blue-600 font-medium">Eliza Fury</h1>
           <h3 className="text-2xl py-2">Developer and designer</h3>
         <p className="text-md py-5 leading-8 text-gray-400">
-          Passionate developer focused on accessibility and creative coding.
-          Look at my portfolio and don't hesitate to contact me.
+        Passionate developer with a data-driven approach, prioritizing accessibility and inclusivity. 
+        Blend of creativity and technical expertise from an advanced diploma in fashion and technology. 
+        Committed to user empathy and creating seamless experiences.
         </p>
         </div>
         <div className="text-5xl flex justify-center gap-16 py-3 text-gray-400">
-        <AiFillLinkedin></AiFillLinkedin>
-        <AiFillYoutube></AiFillYoutube>
+        <a href='https://www.linkedin.com/in/eliza-fury-3004b3110/'><button><AiFillLinkedin></AiFillLinkedin></button></a>
+        <a href='https://www.youtube.com/channel/UCCwlIqh6VjXceir9BHMl_YQ'><button><AiFillYoutube></AiFillYoutube></button></a>
+        <a href='https://medium.com/@ejfury'><button><FaMedium></FaMedium></button></a>
         </div>
         <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
           <Image src={eliza} alt="Eliza Fury" />
         </div>
+        </section>
+        <section>
+    
+        </section>
     </main>
   )
 }
