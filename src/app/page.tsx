3d5333d517fba115471e010761bbf163f431e5e0
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2023-06-27 00:09:53 +1000

    Add profile picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import Head from "next/head";
+import {AiFillLinkedin, AiFillYoutube}  from "react-icons/ai";
+import eliza from "./eliza.jpeg"
 
 export default function Home() {
   return (
@@ -10,10 +12,22 @@ export default function Home() {
             <li></li>
             <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 rounded-md ml-8" href="#">Resume</a></li>
           </ul>
-          
         </nav>
-
-   
+        <div className="text-center p-10">
+          <h1 className="text-5xl py-2 text-teal-600 font-medium">Eliza Fury</h1>
+          <h3 className="text-2xl py-2">Developer and designer</h3>
+        <p className="text-md py-5 leading-8 text-gray-400">
+          Passionate developer focused on accessibility and creative coding.
+          Look at my portfolio and don't hesitate to contact me.
+        </p>
+        </div>
+        <div className="text-5xl flex justify-center gap-16 py-3 text-gray-400">
+        <AiFillLinkedin></AiFillLinkedin>
+        <AiFillYoutube></AiFillYoutube>
+        </div>
+        <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+          <Image src={eliza} alt="Eliza Fury" />
+        </div>
     </main>
   )
 }
