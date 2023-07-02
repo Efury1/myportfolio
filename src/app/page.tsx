dcef0bbb65bebc059e32acf86fa630c052027c1b
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2023-07-02 20:48:16 +1000

    Change colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main>
         <nav className="py-10 mb-12 flex justify-between">
-          <h1 className="text-xl">Eliza Fury</h1>
+          <h1 className="text-xl ">Eliza Fury</h1>
           <ul className="flex items-center">
             <li></li>
             <li><a className="bg-gradient-to-r from-blue-500 to-purple-500 text-white px-4 rounded-md ml-8" href="https://www.linkedin.com/in/eliza-fury-3004b3110/">Resume</a></li>
@@ -16,15 +16,15 @@ export default function Home() {
         </nav>
         <section>
         <div className="text-center p-10">
-          <h1 className="text-5xl py-2 text-blue-600 font-medium">Eliza Fury</h1>
+          <h1 className="text-5xl py-2 text-teal-600 font-medium">Eliza Fury</h1>
           <h3 className="text-2xl py-2">Developer and designer</h3>
-        <p className="text-md py-5 leading-8 text-gray-400">
+        <p className="text-md py-5 leading-8 text-white-800">
         Passionate developer with a data-driven approach, prioritizing accessibility and inclusivity. 
         Blend of creativity and technical expertise from an advanced diploma in fashion and technology. 
         Committed to user empathy and creating seamless experiences.
         </p>
         </div>
-        <div className="text-5xl flex justify-center gap-16 py-3 text-gray-400">
+        <div className="text-5xl flex justify-center gap-16 py-3 text-black-400">
         <a href='https://www.linkedin.com/in/eliza-fury-3004b3110/'><button><AiFillLinkedin></AiFillLinkedin></button></a>
         <a href='https://github.com/Efury1?tab=overview&from=2023-06-01&to=2023-06-27'><button><FaGithubAlt></FaGithubAlt></button></a>
         <a href='https://www.youtube.com/channel/UCCwlIqh6VjXceir9BHMl_YQ'><button><AiFillYoutube></AiFillYoutube></button></a>
