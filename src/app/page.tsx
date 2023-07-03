307e75daa8d5a0667886bf8e880dc422cea1dd55
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2023-07-03 17:38:17 +1000

    Update style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,33 +11,32 @@ export default function Home() {
           <h1 className="text-xl">Eliza Fury</h1>
           <ul className="flex items-center">
             <li></li>
-            <li><a className="bg-gradient-to-r from-blue to-purple text-white px-4 rounded-md ml-8" href="https://www.linkedin.com/in/eliza-fury-3004b3110/">My Resume</a></li>
+            <li><a className="bg-white hover:bg-pink hover:text-white text-blue font-semibold py-2 px-1 border border-gray-200 rounded shadow" href="https://www.linkedin.com/in/eliza-fury-3004b3110/">My Resume</a></li>
           </ul>
         </nav>
         <section>
-        <div className="text-center p-10">
-          <p className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block bg-pink">
-          <h1 className="text-5xl py-2 text-white font-medium">Eliza Fury</h1></p>
-          <h3 className="text-2xl py-2">Developer and designer</h3>
-        <p className="text-md py-5 leading-8 text-white-800">
-        Passionate developer with a data-driven approach, prioritizing accessibility and inclusivity. 
-        Blend of creativity and technical expertise from an advanced diploma in fashion and technology. 
-        Committed to user empathy and creating seamless experiences.
-        </p>
-        </div>
-        <div className="text-5xl flex justify-center gap-16 py-3 text-black-400">
-        <a href='https://github.com/Efury1?tab=overview&from=2023-06-01&to=2023-06-27'><button><FaGithubAlt></FaGithubAlt></button></a>
-        <a href='https://www.youtube.com/channel/UCCwlIqh6VjXceir9BHMl_YQ'><button><AiFillYoutube></AiFillYoutube></button></a>
-        <a href='https://medium.com/@ejfury'><button><FaMedium></FaMedium></button></a>
-        </div>
-        <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
-          <Image src={eliza} alt="Eliza Fury" />
-        </div>
-        </section>
+          <div className="text-center p-10">
+            <p className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block bg-pink">
+            <h1 className="text-5xl py-2 text-white font-medium"> Eliza Fury</h1></p>
+            <h3 className="text-2xl py-2">Developer and designer</h3>
+              <p className="text-md py-5 leading-8 text-white-800">
+              Passionate developer with a data-driven approach, prioritizing accessibility and inclusivity. 
+              Blend of creativity and technical expertise from an advanced diploma in fashion and technology. 
+              Committed to user empathy and creating seamless experiences.
+              </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-black-400">
+            <a href='https://github.com/Efury1?tab=overview&from=2023-06-01&to=2023-06-27'><button><FaGithubAlt></FaGithubAlt></button></a>
+            <a href='https://www.youtube.com/channel/UCCwlIqh6VjXceir9BHMl_YQ'><button><AiFillYoutube></AiFillYoutube></button></a>
+            <a href='https://medium.com/@ejfury'><button><FaMedium></FaMedium></button></a>
+          </div>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden ">
+            <Image src={eliza} alt="Eliza Fury" />
+          </div>
+          </section>
         <section>
-        <div className="columns-2 md:columns-2 grid grid-cols-2 ">
+        <div className="columns-3 md:columns-3 grid grid-cols-3 ">
           <div className="group block max-w-xs mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500 pd-5 my-5">
-                <h3 className="text-3xl flex justify-center gap-1 py-1">My Skills</h3>
                 <h3 className="text-2xl py-1 text-pink">Development</h3>
                 <p className="text-black">As a frontend developer, I have knowledge of: </p>
                   <h4 className="py-2 text-blue">HTML, CSS and  Javascript</h4> 
@@ -54,6 +53,14 @@ export default function Home() {
                   <p className="py-2 text-blue">Illustrator</p>
                   <p className=" text-blue">Photoshop</p>
               </p>
+            </div>
+
+            <div className="group block max-w-xs mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500 pd-5 my-5">
+                <h3 className="text-2xl py-1 text-pink">Analytics</h3>
+                <p className="text-black">This involves: </p>
+                  <h4 className="py-2 text-blue">Data Visualisation</h4> 
+                  <h4 className="py-2 text-blue">Ability to Anaylse business practices and system functionality</h4> 
+                  <h4 className="py-2 text-blue">Make data-driven decisions</h4> 
             </div>
           </div>
         </section>
