a75d9e3d75fe5191adeb3178b407a0f9b26ed1b7
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2023-09-12 13:10:29 +1000

    Add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Head from "next/head";
 import {AiFillLinkedin, AiFillYoutube}  from "react-icons/ai";
 import { FaGithubAlt, FaMedium } from "react-icons/fa";
-import eliza from "./eliza2.png"
+import eliza from "./eliza2.png";
 
 export default function Home() {
   return (
@@ -60,6 +60,36 @@ export default function Home() {
             </div>
           </div>
         </section>
+        <footer className="footer footer-center w-full p-4 bg-pink">
+      <div className="text-center">
+          <a className="relative bg-pink-200 pt-8 pb-6" href="ejfury@protonmail.com">
+          <div className="container mx-auto px-4">
+          <div className="flex flex-wrap text-left lg:text-left">
+            <div className="w-full lg:w-6/12 px-4">
+                <div className="container mx-auto px-4">
+                  <h4 className="text-3xl text-white">Need other things to explore?</h4>
+                  <h5 className="text-lg mt-0 mb-2 text-white">
+                  Don't hesitate to look through the footer.
+                </h5>
+                </div>
+              </div>
+              </div>
+            </div>
+            <div className="w-full lg:w-6/12 px-4 ">
+              <div className="flex flex-wrap items-top mb-6">
+                <div className="w-full lg:w-4/12 px-4 ml-auto">
+                <span className="block uppercase text-sm font-semibold mb-2 text-right lg:text-right text-white">Other pages</span>
+                <ul className="list-unstyled">
+                  <li>
+                    <a href="page2.tsx" className="font-semibold block pb-2 text-sm text-right lg:text-right text-white">Coming soon</a>
+                  </li>
+                </ul>
+                </div>
+              </div>
+            </div>
+            </a>
+      </div>
+    </footer>
     </main>
   )
 }
