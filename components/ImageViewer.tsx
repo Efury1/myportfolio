829c014eea7308052012c2390705f6f88733f22c
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2024-07-04 08:04:30 +1000

    Change display for mobile

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -21,11 +21,13 @@ const ImageViewer: React.FC<ImageViewerProps> = ({
         <Image
           src={selectedImage}
           alt={selectedImage}
-          width={500} // Specify a fixed width
-          height={300} // Specify a fixed height
+          width={500} 
+          height={300} 
           className="image py-2"
         />    
-        <p className="description">{description}</p>
+        <div className="group block max-w-xs mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500 pd-5 my-5">
+          <p className="description">{description}</p>
+        </div>
         <button
           className="absolute top-1/2 transform -translate-y-1/2 left-0 text-white py-14 px-4 md:py-28 md:px-5 text-3xl md:text-5xl"
           onClick={onPrev}
@@ -44,3 +46,5 @@ const ImageViewer: React.FC<ImageViewerProps> = ({
 };
 
 export default ImageViewer;
+
+
