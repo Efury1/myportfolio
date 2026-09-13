bdad26ed99a4697a8b5bd4c322d552212f7a3d89
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2026-09-13 19:48:11 +0100

    Add spacing to button

--- src/app/components/RetroWindow.tsx
+++ src/app/components/RetroWindow.tsx
@@ -9,11 +9,11 @@ export default function RetroWindow({ title, children, color = '#000080', classN
     return (
         <div className={`border border-black shadow-lg bg-white w-fit ${className ?? ''}`}>
             <div
-                className="w-full px-2 py-1 flex items-center justify-between"
+                className="w-full px-2 py-1 flex items-center justify-between gap-2"
                 style={{ backgroundColor: color }}
             >
-                <span className="text-black ext-xs font-bold uppercase">{title}</span>
-                <button className="bg-white text-black text-xs border border-black leading-none w-4 h-4 flex items-center justify-center">
+                <span className="text-black text-xs font-bold uppercase truncate">{title}</span>
+                <button className="bg-white text-black text-xs border border-black leading-none w-4 h-4 flex items-center justify-center shrink-0 ml-2">
                     X
                 </button>
             </div>
